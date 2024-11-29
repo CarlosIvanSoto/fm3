daef83d8c90b3f75b95aee493d3e4f353306c9b3
Carlos Ivan Soto Perez <gameue@hotmail.com>
2024-11-29 17:59:23 -0600

    Testing: include new routes for app register

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,9 +2,11 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+  let prisma: PrismaService;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -12,17 +14,40 @@ describe('AppController (e2e)', () => {
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    prisma = app.get<PrismaService>(PrismaService);
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  it('GET /', () => {
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
       .expect('Hello World!');
   });
 
-  it('/upload/image (POST)', () => {
+  describe('POST /app/register/test', () => {
+    it('register an test app', async () => {
+      const beforeCount = await prisma.applicationToken.count();
+      const { status, body } = await request(app.getHttpServer())
+        .post('/app/register/test')
+        .send({ secret: 'fm3' });
+      const afterCount = await prisma.applicationToken.count();
+
+      expect(status).toBe(200);
+      expect(body.appName).toBe('test');
+      expect(body.token).toContain('fm3');
+      expect(afterCount - beforeCount).toBe(1);
+    });
+
+    it('fails to register app for duplicate name', () => {
+      return request(app.getHttpServer())
+        .post('/app/register/test')
+        .send({ secret: 'fm3' })
+        .expect(409);
+    });
+  });
+
+  it('POST /upload/image', () => {
     const fileName = 'test.png';
     const response = {
       originImage: `http://localhost:3000/uploads/${fileName}`,
@@ -36,7 +61,7 @@ describe('AppController (e2e)', () => {
       .expect(JSON.stringify(response, null));
   });
 
-  it('/upload/logo (POST)', () => {
+  it('POST /upload/logo', () => {
     const fileName = 'test.png';
     const dest = process.env.LOGO_DEST ?? 'logos';
     const response = {
